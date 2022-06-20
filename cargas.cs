47a7e78d2693aa63cb5e716873fb2cb3df62c7b2
nicolas philbois <102417634+philbois@users.noreply.github.com>
2022-06-20 04:43:08 -0300

    Update cargas.cs

--- cargas.cs
+++ cargas.cs
@@ -9,33 +9,59 @@ namespace trasporte_TP3
 {
     internal class cargas
     {
-       
+        static int contador;
         private int id;
         private string nombre;
         private double enviob = 5.25;
         private double eFull = 8.95;
         private int tipoA, tipoB, tipoC;
         DateTime hora;
-Int peso=1000, pesoEntrada, difpeso; //agregarcion
+        int peso=1000, pesoEntrada, difpeso; //agregarcion
         double costo;
         bool feriado;
+        double porc;
+        bool multa = false;
         public cargas(string nom, int id, int tipoA,int tipoB, int tipoC, DateTime tiempo, bool feriado)
         {
+            contador++;// variable que cuenta los contenedores
             this.Id = id;
             this.Nombre = nom;
             
             hora = tiempo;
-            this.feriado=feriado;
-pesoEntrada=(5*tipoA) +(15* tipoB) +(25* tipoC);
-difpeso= peso- pesoEntrada;
+         
+            pesoEntrada=(5*tipoA) +(15* tipoB) +(25* tipoC);
+            difpeso= peso- pesoEntrada;
 
-            if(difpeso<-100)
-{
-// agrege esto
-}
-            if(hora.DayOfWeek == DayOfWeek.Sunday)
+            if (difpeso > -100 && difpeso < -10)
             {
-                
+                porc = 7;
+            }else if (difpeso > -10)
+            {
+                porc = -5;
+            }else if(difpeso >5 && difpeso <= 50)
+            {
+                porc = 18;
+            }else if( difpeso > 50)
+            {
+                porc = 80;
+                multa = true;
+            }
+            else
+            {
+                porc = 10;
+            }
+      
+            if(hora.DayOfWeek == DayOfWeek.Sunday || feriado==true)
+            {
+                //agregar mil al precio
+            }
+            if (hora.Hour >= 6 && hora.Hour<=20)
+            {
+                descuento = -5;
+            }
+            if (!hora.Hour > 6 && hora.Hour <= 20)
+            {
+                descuento = +4;
             }
 
         }
@@ -45,9 +71,7 @@ difpeso= peso- pesoEntrada;
         }
         public int Id { get => id; set => id = value; }
         public string Nombre { get => nombre; set => nombre = value; }
-        public int TipoA { get => tipoA; set => tipoA = value; }
-        public int TipoB { get => tipoB; set => tipoB = value; }
-        public int TipoC { get => tipoC; set => tipoC = value; }
+
         public int Hora()
         {
             int h = Convert.ToInt32(hora.Hour);
